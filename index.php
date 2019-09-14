a017b2d0ba8b008dea0f536ffbc5caf5354fe714
Ario <dev@ariochain.info>
2019-09-15 01:27:56 +0200

    nette index

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
+if (file_exists(__DIR__ . '/../app/bootstrap.php')) {
+
 $container = require __DIR__ . '/../app/bootstrap.php';
 
 $container->getByType(Nette\Application\Application::class)
 		->run();
 
+    } else {
+    
+        require_once __DIR__.'/node-index.php';
+       
+    }
